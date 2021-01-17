3a45b6251846bc33a385d01bfed44b8e07297f57
UltraDev <petzmagnus@gmail.com>
2021-01-17 15:11:48 +0100

    Added Click and Hover support to ConverterSpBg

--- src/main/java/net/ultragrav/command/wrapper/chat/impl/ConverterSpBg.java
+++ src/main/java/net/ultragrav/command/wrapper/chat/impl/ConverterSpBg.java
@@ -2,10 +2,13 @@ package net.ultragrav.command.wrapper.chat.impl;
 
 import net.kyori.text.Component;
 import net.kyori.text.TextComponent;
+import net.kyori.text.event.ClickEvent;
+import net.kyori.text.event.HoverEvent;
 import net.kyori.text.format.TextColor;
 import net.kyori.text.format.TextDecoration;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 public class ConverterSpBg {
     public static BaseComponent toSpBg(Component comp) {
@@ -26,7 +29,34 @@ public class ConverterSpBg {
     private static net.md_5.bungee.api.chat.TextComponent convertTextComponent(TextComponent comp) {
         net.md_5.bungee.api.chat.TextComponent ret = new net.md_5.bungee.api.chat.TextComponent(comp.content());
         writeColor(comp, ret);
+        writeEvents(comp, ret);
         return ret;
+    }
+
+    private static void writeEvents(Component source, BaseComponent target) {
+        @Nullable HoverEvent hover = source.hoverEvent();
+        if (hover != null) {
+            target.setHoverEvent(convertHover(hover));
+        }
+
+        ClickEvent click = source.clickEvent();
+        if (click != null) {
+            target.setClickEvent(convertClick(click));
+        }
+    }
+
+    private static net.md_5.bungee.api.chat.HoverEvent convertHover(HoverEvent hover) {
+        return new net.md_5.bungee.api.chat.HoverEvent(convertHoverAction(hover.action()), new BaseComponent[]{toSpBg(hover.value())});
+    }
+    private static net.md_5.bungee.api.chat.HoverEvent.Action convertHoverAction(HoverEvent.Action action) {
+        return net.md_5.bungee.api.chat.HoverEvent.Action.valueOf(action.name());
+    }
+
+    private static net.md_5.bungee.api.chat.ClickEvent convertClick(ClickEvent click) {
+        return new net.md_5.bungee.api.chat.ClickEvent(convertClickAction(click.action()), click.value());
+    }
+    private static net.md_5.bungee.api.chat.ClickEvent.Action convertClickAction(ClickEvent.Action action) {
+        return net.md_5.bungee.api.chat.ClickEvent.Action.valueOf(action.name());
     }
 
     private static void writeColor(Component source, BaseComponent target) {
